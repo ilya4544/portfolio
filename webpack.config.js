8f9977459763cd24827e139bcd9d25d4bfe0c634
Ilya Lyamkin <ilya.lyamkin@gmail.com>
2016-06-07 15:50:48 +0300

    Enable adding custom fonts, refactor the Experience section template and style, applied brand new font to the main theme

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,8 @@ module.exports = {
             { test: /\.css$/, exclude: /node_modules/, loaders: ["style", "css", "postcss"] },
             { test: /\.scss$/, exclude: /node_modules/, loaders: ["style", "css", "postcss", "sass"] },
             { test: /\.(png|jpg)$/, exclude: /node_modules/, loader: 'file' },
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/, loader : 'file-loader'}
         ]
     },
     postcss: function() {
